4b3f5d89d8cf13793769ee0e001e62353baf4bb5
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-02 16:47:00 +0800

    tambah fungsi output untuk menampilkan data array

--- mergesort.cpp
+++ mergesort.cpp
@@ -33,3 +33,13 @@ void input()
         cin >> arr[i];
     }
 }
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
