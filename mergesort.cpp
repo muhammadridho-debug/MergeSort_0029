1718ae9be9d59d9badf74f4657f0ac65e7862ea9
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-02 16:48:31 +0800

    buat kerangka mergeSort dan tentukan batas henti rekursi (base case)

--- mergesort.cpp
+++ mergesort.cpp
@@ -34,6 +34,17 @@ void input()
     }
 }
 
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
+}
+
 void output()
 {
     cout << "\nData setelah diurutkan (Merge Sort): ";
